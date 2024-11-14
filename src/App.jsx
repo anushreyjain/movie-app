abccf32bb57e67026690f3d9d91f708d93fe0d2e
Anushrey <anushrey.jain@napses.com>
2024-11-14 23:21:54 +0530

    fixed height issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import MovieList from "./Components/MobieList";
 
 const App = () => {
   return (
-    <div className="w-full flex items-center justify-center bg-neutral-50">
+    <div className="w-full flex justify-center items-start bg-neutral-50 min-h-screen">
       <MovieList />
     </div>
   );
